c21bbd15a40661ddfa07c029dadd9a4007f2bfd9
Luca Carlon <carlon.luca@gmail.com>
2017-01-23 14:12:50 +0100

    Fixed key lookup when looking for swift callback.

--- SwiftWebViewBridge/SwiftWebViewBridge.swift
+++ SwiftWebViewBridge/SwiftWebViewBridge.swift
@@ -181,14 +181,17 @@ extension SwiftWebViewBridge {
         
         for swvbMsg in messages {
             // Swift callback(after JS finished designated handler called by Swift)
-            if let responseId = swvbMsg["responseId"] as? String {
-                if let callback = self.jsCallbacks[responseId] {
-                    let responseData = swvbMsg["responseData"] != nil ? swvbMsg["responseData"] : NSNull()
-                    callback(responseData!)
-                    self.jsCallbacks.removeValueForKey(responseId)
-                }
-                else {
-                    self.swvb_printLog(.ERROR("No matching callback closure for: \(swvbMsg)"))
+            if let swvbMsgData = swvbMsg["data"] as? [String: AnyObject] {
+                if let responseId = swvbMsgData["responseId"] as? String {
+                    if let callback = self.jsCallbacks[responseId] {
+                        if let responseData = swvbMsgData["responseData"] != nil ? swvbMsgData["responseData"] : NSNull() {
+                            callback(responseData as! NSDictionary)
+                        }
+                        self.jsCallbacks.removeValue(forKey: responseId)
+                    }
+                    else {
+                        self.swvb_printLog(.ERROR("No matching callback closure for: \(swvbMsg)"))
+                    }
                 }
             }
             else { // JS call Swift Handler
